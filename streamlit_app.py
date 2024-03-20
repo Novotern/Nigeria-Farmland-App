d3f98927ce1c331d346c91a6acbe9089af3ef0f5
TelRich <okorogoodrich@gmail.com>
2024-03-20 12:02:10 +0100

    change naming

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ farmland = pd.read_csv("farmlands.csv")
 # Display the distribution of farm types
 farm_type_distribution = farmland['type'].value_counts().reset_index()
 
-fig1 = px.bar(farm_type_distribution, 'index', 'type', 
-        labels={'index': '<b>Farm Type</b>', 'type':'<b>Counts</b>'},
+fig1 = px.bar(farm_type_distribution, 'type', 'count', 
+        labels={'type': '<b>Farm Type</b>', 'count':'<b>Counts</b>'},
         title='<b>Distribution of Farm Types</b>', width=700)
 with st.expander('Farm Type Distribution', True):
     st.plotly_chart(fig1, use_container_width=True)
