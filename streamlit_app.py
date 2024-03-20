ae1eb2a28fd6a4377be5509bf9a3329ee37ebda2
TelRich <okorogoodrich@gmail.com>
2024-03-20 11:55:05 +0100

    sample chart on app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,23 @@ import streamlit as st
 
 
 st.title("Nigeria Farmland Analysis")
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+farmland = pd.read_csv("farmlands.csv")
+
+
+# Display the distribution of farm types
+farm_type_distribution = farmland['type'].value_counts().reset_index()
+
+fig1 = px.bar(farm_type_distribution, 'index', 'type', 
+        labels={'index': '<b>Farm Type</b>', 'type':'<b>Counts</b>'},
+        title='<b>Distribution of Farm Types</b>', width=700)
+with st.expander('Farm Type Distribution', True):
+    st.plotly_chart(fig1, use_container_width=True)
+    if st.checkbox('Show raw data'):
+        st.write(farm_type_distribution)
