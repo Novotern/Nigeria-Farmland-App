446f4d6b83d233e360fd246de09b518bac642926
TelRich <okorogoodrich@gmail.com>
2024-03-22 09:21:29 +0100

    added description

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,14 @@ st.set_page_config(layout="wide", page_title='NOVOTERN PORTFOLIO', page_icon='lo
 
 st.title("Nigeria Farmland Analysis")
 
+st.markdown("""
+
+This project embarks on an exploration of Nigeria farmland data, with a specific focus on crucial agricultural processes essential for food production sustainability. 
+We aim to unravel the distribution of farm types, identify primary agricultural focuses, and evaluate the prevalence of processing facilities within these agricultural landscapes.
+Through meticulous analysis and data-driven exploration, we endeavor to provide an overview of farmland data, facilitating informed decision-making, and fostering deeper insights into the intricacies of agricultural systems.       
+            
+            """)
+
 import pandas as pd
 import plotly.express as px
 
